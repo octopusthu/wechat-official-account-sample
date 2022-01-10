4eb2bb0db7795e2ea877db8dd63f98e0d817c8bd
octopusthu-at-mbptt <octopusthu@gmail.com>
2022-01-10 20:45:34 +0800

    WIP - to be squashed

---
+++ README.md
@@ -0,0 +1,4 @@
+# 微信公众号示例程序
+
+1. 下载 [微信开发者工具](https://developers.weixin.qq.com/miniprogram/dev/devtools/download.html) 。请注意，需要选择 `预发布版 RC Build`。
+1. 使用微信开发者工具导入此项目
---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+const router = express.Router();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+router.get('/ciam-test', function (req, res) {
+  res.sendFile(path.join(__dirname + '/ciam-test.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
